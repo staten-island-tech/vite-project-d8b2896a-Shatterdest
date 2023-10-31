c9987dfca9822bb28b929855e8fc312d9e0df9f3
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2023-10-31 13:13:52 -0400

    I GOT IT TO WORK!!!!!!!!!!!!!!!!!!!!

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,14 @@ import { getData } from './data.js'
 const data = JSON.stringify(getData())
 console.log(data)
 
+const selectors = {
+    form: document.getElementById('form'),
+    submit: document.getElementById('submit')
+}
+console.log(selectors)
+
+selectors.form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('runnin!')
+
+})
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["src/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./js/*{.js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
