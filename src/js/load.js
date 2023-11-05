7fccdec6af67ab04a1b7626bfbde99ab0d3d5dcc
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2023-11-05 01:05:55 -0500

    BOOM

--- src/js/load.js
+++ src/js/load.js
@@ -1,7 +1,8 @@
 export function load(data, s) {
     for(let i=0; i<data.length; i++) {
-        const cardLayout = `    <div class="card">
+        const cardLayout = `<div class="card">
         <h3 class="display-name">${data[i].displayName}</h3>
+        <img src="${data[i].displayIconSmall}" class='img'>
         <h3 class="agent-type">${data[i].role.displayName}</h3>
         <p class="description">${data[i].description}</p>
         </div>`;
