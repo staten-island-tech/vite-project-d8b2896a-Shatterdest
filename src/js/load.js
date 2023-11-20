5488f187094e70a882c17842edbea8e342ea21ce
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2023-11-20 12:58:41 -0500

    FINISHED HOPEFULYERKLJFIOSDFIOSDJFIOASDJIOASDFIOP

--- src/js/load.js
+++ src/js/load.js
@@ -3,7 +3,7 @@ export function load(data, s) {
     const cardLayout = `<div class="card">
         <h3 class="display-name">${data[i].displayName}</h3>
         <img src="${data[i].displayIconSmall}" class='img'>
-        <img src="${data[i].role.displayIcon}" class='img'><h3 class="agent-type">${data[i].role.displayName}</h3>
+        <img src="${data[i].role.displayIcon}" class='class-img'><h3 class="agent-type">${data[i].role.displayName}</h3>
         <p class="cost">${data[i].cost} Credits</p>
         <p class="description">${data[i].description}</p>
         </div>`;
