9d2a99c209633730b2e386c34846ebe8e78b299a
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2023-11-20 13:13:28 -0500

    fix WAVE errors

--- src/js/load.js
+++ src/js/load.js
@@ -2,8 +2,8 @@ export function load(data, s) {
   for (let i = 0; i < data.length; i++) {
     const cardLayout = `<div class="card">
         <h3 class="display-name">${data[i].displayName}</h3>
-        <img src="${data[i].displayIconSmall}" class='img'>
-        <img src="${data[i].role.displayIcon}" class='class-img'><h3 class="agent-type">${data[i].role.displayName}</h3>
+        <img src="${data[i].displayIconSmall}" class='img' alt='This card is about: ${data[i].displayName}'>
+        <img src="${data[i].role.displayIcon}" class='class-img' alt='Agent Class is: ${data[i].role.displayName}'><h3 class="agent-type">${data[i].role.displayName}</h3>
         <p class="cost">${data[i].cost} Credits</p>
         <p class="description">${data[i].description}</p>
         </div>`;
